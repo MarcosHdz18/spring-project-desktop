1d3004f783b9ea14d04188f3978394e421e1ca35
Marcos Salvador Hernández Guadarrama <marcoshernandezg18@yahoo.com>
2021-06-16 00:41:46 -0500

    Proyecto: spring-project-desktop, Descripción: Se agrega Spring Framework y se realiza configuración.

---
+++ src/main/java/com/devmark/spring_project_desktop/App.java
@@ -0,0 +1,13 @@
+package com.devmark.spring_project_desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/devmark/springprojectdesktop/test/SpringDesktopTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.devmark.springprojectdesktop.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author marcos.hernandez
+ *
+ */
+class SpringDesktopTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
