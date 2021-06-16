d12d95987ed230c3bd3ba3727546f4ab91602130
Marcos Salvador Hernández Guadarrama <marcoshernandezg18@yahoo.com>
2021-06-16 00:41:46 -0500

    Proyecto: spring-project-desktop, Descripción: Versión inicial del proyecto.

---
+++ src/test/java/com/devmark/spring_project_desktop/AppTest.java
@@ -0,0 +1,20 @@
+package com.devmark.spring_project_desktop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
